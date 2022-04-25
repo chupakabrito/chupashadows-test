2ce8255f26b9fa11a8d066d9717cc56a59f76377
chupakabrito <elchupakabrito@gmail.com>
2022-04-25 15:04:56 +0100

    just playing

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 import { chupashadows } from 'chupashadows';
+import {
+    atan2, chain, derivative, e, evaluate, log, pi, pow, round, sqrt
+  } from 'mathjs';
+
+  let price = "http://192.168.1.39:3002/api/price"
+  let displayBtcUsdPrice = document.getElementById('btcusdprice');
+  let displayBtcGbpPrice = document.getElementById('btcgbpprice');
+  let displayBtcEurPrice = document.getElementById('btceurprice');
+
+  fetch(price)
+  .then((response)=> {
+      return response.json();
+  })
+  .then((data)=> {
+      let price = data;
+      let btcUsdPrice = price.usd;
+      let btcGbpPrice = price.gbp;
+      let btcEurPrice = price.eur;
+
+      displayBtcUsdPrice.innerHTML = btcUsdPrice;
+      displayBtcGbpPrice.innerHTML = btcGbpPrice;
+      displayBtcEurPrice.innerHTML = btcEurPrice;
+  })
+  .catch();
+
+
 
 chupashadows({
     shadow_type: 'hard',
-    padding: true
+    padding: false
 })
