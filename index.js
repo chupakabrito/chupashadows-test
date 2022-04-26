4a5b9629fc7bbb1de4fd8f356c6059fdddc21155
chupakabrito <elchupakabrito@gmail.com>
2022-04-26 01:49:08 +0100

    change the ip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {
     atan2, chain, derivative, e, evaluate, log, pi, pow, round, sqrt
   } from 'mathjs';
 
-  let price = "http://192.168.1.39:3002/api/price"
+  let price = "http://192.168.1.145:3002/api/price"
   let displayBtcUsdPrice = document.getElementById('btcusdprice');
   let displayBtcGbpPrice = document.getElementById('btcgbpprice');
   let displayBtcEurPrice = document.getElementById('btceurprice');
